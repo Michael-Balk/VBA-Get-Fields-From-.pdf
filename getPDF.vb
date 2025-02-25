43bb06dee5b3944e69b795ca069029e6694582f3
Michael S. Balk <136604154+Michael-Balk@users.noreply.github.com>
2025-02-25 04:02:54 -0600

    Add files via upload

---
+++ getPDF.vb
@@ -0,0 +1,38 @@
+﻿Imports Microsoft.VisualBasic
+Imports System.Runtime.InteropServices.JavaScript
+
+Public Class getPDF
+    Public Function getFormData(formLocation As String) As Array
+
+        Dim AcrobatApp As Object
+        Dim thePDF As Object
+        Dim javascriptObj As Object
+
+        AcrobatApp = CreateObject("AcroExch.App")
+        thePDF = CreateObject("AcroExch.PDDoc")
+
+        thePDF.Open(formLocation)
+
+        javascriptObj = thePDF.GetJSObject
+
+        Dim num As Integer
+        num = javascriptObj.numFields
+
+        MsgBox("The Number of Fields is: " + CStr(num))
+
+        Dim result(num - 1) As Object
+
+        For i = 0 To num - 1
+            result(i) = javascriptObj.getField(javascriptObj.getNthFieldName(i)).Value
+        Next i
+
+        thePDF.Close
+        AcrobatApp.Exit
+        AcrobatApp = Nothing
+        thePDF = Nothing
+
+
+        getFormData = result
+
+    End Function
+End Class
